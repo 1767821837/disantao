f60f71bda98a7febabb02c1a7de955ee6948873c
22805 <2280514662@qq.com>
2019-04-11 12:10:07 +0800

    WANGSHUAI

--- app/src/main/java/cn/logcode/traffic/ui/activity/MainActivity.java
+++ app/src/main/java/cn/logcode/traffic/ui/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
